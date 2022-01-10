93d9d5337b28cf14d7bd2f78c25b2bdc08f285f4
Alexander Schroyer <81829512+alexanderschroyer@users.noreply.github.com>
2022-01-10 12:33:09 -0600

    link to article tag

--- src/components/recipients/RecipientList.js
+++ src/components/recipients/RecipientList.js
@@ -120,7 +120,7 @@ export const RecipientList = () => {
                         {searchResults?.shopping_results?.map(result => {
                             return <div id="search__right">
                                 <a className="search__title">{result.title}</a>
-                                <Link className="search__link" target="_blank" to={result.product_link}>{result.product_link}</Link>
+                                <a className="search__link" target="_blank" to={result.product_link}>{result.product_link}</a>
                                 <a>{result.price}</a>
                                 <img className="search__img" src={result.thumbnail} />
                             </div>
